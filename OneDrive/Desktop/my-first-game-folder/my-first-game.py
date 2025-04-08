abada7cd810d2463c921ea1e403da641555383c1
matty-boles <mb1357@exeter.ac.uk>
2025-04-08 23:24:49 +0100

    boundaries!

--- OneDrive/Desktop/my-first-game-folder/my-first-game.py
+++ OneDrive/Desktop/my-first-game-folder/my-first-game.py
@@ -16,39 +16,46 @@ clock = pygame.time.Clock()
 minion_img_unscaled = pygame.image.load('minion2.png')
 minion_img = pygame.transform.scale(minion_img_unscaled, (180,300))
 
+minion_width = 175
+
 def minion(x,y):
     gameDisplay.blit(minion_img, (x,y))
 
-x = (display_width * 0.38)
-y = (display_height * 0.7)
+def game_loop():
+    x = (display_width * 0.38)
+    y = (display_height * 0.7)
 
-x_change = 0
-
-
-crashed = False
-
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            if event.key == pygame.K_RIGHT:
-                x_change = 5
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
+    x_change = 0
 
 
-    x += x_change
+    gameExit = False
 
-    gameDisplay.fill(white)
-    minion(x,y)
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
 
-    pygame.display.update()
-    clock.tick(60)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
 
+
+        x += x_change
+
+        gameDisplay.fill(white)
+        minion(x,y)
+
+        if x > display_width - minion_width or x < 0:
+            gameExit = True
+
+        pygame.display.update()
+        clock.tick(60)
+
+game_loop()
 pygame.quit()
 quit()
